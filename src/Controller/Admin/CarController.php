88278259310d9a1a5f4f11413e847a005eecf673
maciek <maciej.sloniewski@projectic.pl>
2019-12-09 23:44:14 +0100

    inital commit

---
+++ src/Controller/Admin/CarController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\CarRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class CarController
+ * @package App\Controller
+ * @Route("/admin/cars", name="admin_cars_")
+ */
+class CarController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     */
+    public function index(CarRepository $cars)
+    {
+        return $this->render('admin/cars/index.html.twig');
+    }
+
+    /**
+     * @Route("/create", name="create", methods={"GET", "POST"})
+     **/
+    public function create()
+    {
+
+    }
+}
